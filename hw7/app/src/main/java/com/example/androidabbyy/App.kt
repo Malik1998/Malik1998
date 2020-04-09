7b6c97bdcacec2341bb31b140f36222456f3802d
Malik1998 <gazizullin.malik2010@yandex.ru>
2020-04-09 14:51:14 +0300

    Sem2, hw5

---
+++ hw7/app/src/main/java/com/example/androidabbyy/App.kt
@@ -0,0 +1,29 @@
+package com.example.androidabbyy
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import com.example.androidabbyy.db.DatabaseHolder
+
+
+@SuppressLint("Registered")
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        context = this
+        databaseHolder = DatabaseHolder(context as App)
+    }
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        private var context: Context? = null
+        var databaseHolder: DatabaseHolder? = null
+            private set
+
+        // Способ получения контекста из любой части приложения
+        fun getContext(): Context? {
+            return context
+        }
+
+    }
+}
